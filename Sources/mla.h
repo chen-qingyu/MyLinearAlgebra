447dc07d7d6c02eaf3f94a1645270415fb51102f
chen-qingyu <236521827@qq.com>
2023-11-01 15:32:30 +0800

    添加mla.hpp注释和版本检查

--- Sources/mla.h
+++ Sources/mla.h
@@ -1,7 +1,21 @@
+/**
+ * @file mla.h
+ * @author 青羽 (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
+ * @brief Unified header file.
+ * @version 0.1
+ * @date 2023.11.01
+ *
+ * @copyright Copyright (c) 2023
+ */
+
 #ifndef MLA_H
 #define MLA_H
 
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
+
 #include "Matrix.h"
 #include "Vector.h"
+
+#endif // C++17
 
 #endif // MLA_H
